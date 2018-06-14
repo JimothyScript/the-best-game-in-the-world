2113c904ca2cbad14bad8d03388ca736fb082fc5
James Kim <yameskim@gmail.com>
2018-06-14 14:23:20 -0400

    Initialize state with populated board
    * Removed setState to avoid double rendering

--- src/components/App.js
+++ src/components/App.js
@@ -7,25 +7,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initialArray: []
+      initialArray: this.initializeGame()
     }
   }
   generateNum() {
     return Math.round(Math.random() * 4);
   }
-  componentDidMount() {
+  initializeGame() {
     // Basically the Game Initializer
     // Populate approximately 25%, which is around 625, out of 2,500 cells programmatically
-    const rowArr = [];
-    let cellArr = [];
     const rowLength = 50;
     const cellLength = 50;
+    const rowArr = [];
+    let cellArr = [];
     // Won't allow more than 25% to be populated
     let maxRandomNum = (rowLength * cellLength) / 4;
+    let populate;
 
     for (let row = 0; row < rowLength; row++) {
       for (let cell = 0; cell < cellLength; cell++) {
-        const populate = this.generateNum();
+        populate = this.generateNum();
         if (populate === 1 && maxRandomNum > 0) {
           cellArr.push(populate);
           maxRandomNum--;
@@ -37,11 +38,16 @@ class App extends Component {
       cellArr = [];
     }
 
-    console.log(rowArr);
+    // this.setState({
+    //   initialArray: [...this.state.initialArray, rowArr]
+    // });
+    return rowArr;
   }
   render() {
+    console.log(this.state.initialArray);
     return (
       <div className="App">
+        <button onClick={() => this.initializeGame()}>Click</button>
       </div>
     );
   }
