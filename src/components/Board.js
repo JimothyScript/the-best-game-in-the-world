b38a8721777c5ab14e5fc0db92b2a91f7535f512
James Kim <yameskim@gmail.com>
2018-06-14 19:23:39 -0400

    Display grid data in the board area

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,21 +6,16 @@ class Board extends Component {
     console.log(this.props.grid);
     return (
       <div className="board-box">
-        <div className="cell-row">
-          <div className="cell-column">1</div>
-          <div className="cell-column">2</div>
-          <div className="cell-column">3</div>
-        </div>
-        <div className="cell-row">
-          <div className="cell-column">4</div>
-          <div className="cell-column">5</div>
-          <div className="cell-column">6</div>
-        </div>
-        <div className="cell-row">
-          <div className="cell-column">7</div>
-          <div className="cell-column">8</div>
-          <div className="cell-column">9</div>
-        </div>
+        {
+          this.props.grid.map((row, i) => {
+            return <div key={i} className="cell-row">{
+                row.map((column, j) => {
+                  return <div key={j} className="cell-column">{column}</div>
+                })
+              }
+            </div>
+          })
+        }
       </div>
     );
   }
