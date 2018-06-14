9f635b02c2231998544c781559e0a7ee44ca4aa8
James Kim <yameskim@gmail.com>
2018-06-14 01:59:53 -0400

    Initial commit

---
+++ READMEGAME.md
@@ -0,0 +1,10 @@
+# The best game in the world
+
+To start the best game in the world:
+
+```
+npm i
+npm start
+```
+
+Then press START in the menu bar!
---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// Will import 2 components: Menu and Board
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
