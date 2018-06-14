a9bafd00ac198255e760206d70ae2dc634bbf3f9
James Kim <yameskim@gmail.com>
2018-06-14 13:53:57 -0400

    Create loop that populates the game board

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,39 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initialArray: Array(50).fill(Array(50).fill(null))
+      initialArray: []
     }
   }
-  test() {
-    console.log(this.state.initialArray);
+  generateNum() {
+    return Math.round(Math.random() * 4);
+  }
+  componentDidMount() {
+    // Basically the Game Initializer
+    // Populate approximately 25%, which is around 625, out of 2,500 cells programmatically
+    const rowArr = [];
+    let cellArr = [];
+    const rowLength = 50;
+    const cellLength = 50;
+    // Won't allow more than 25% to be populated
+    let maxRandomNum = (rowLength * cellLength) / 4;
+
+    for (let row = 0; row < rowLength; row++) {
+      for (let cell = 0; cell < cellLength; cell++) {
+        const populate = this.generateNum();
+        if (populate === 1 && maxRandomNum > 0) {
+          cellArr.push(populate);
+          maxRandomNum--;
+        } else {
+          cellArr.push(null);
+        }
+      }
+      rowArr.push(cellArr);
+      cellArr = [];
+    }
+
+    console.log(rowArr);
   }
   render() {
-    this.test();
     return (
       <div className="App">
       </div>
