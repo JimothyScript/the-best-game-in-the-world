6df1effa444e6256871f3678c819217c5fe153b0
James Kim <yameskim@gmail.com>
2018-07-02 18:06:27 -0400

    Initialize Mocha unit testing

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+describe('Function', () => {
+  describe('Brain', () => {
+    it('should return null if POPULATED cell fewer than 2', () => {
+      // assert.equal()
+    });
+    it('should return 1 if POPULATED cell count between 2 and 3', () => {
+      //
+    });
+    it('should return null if POPULATED cell count greater than 3', () => {
+      //
+    });
+    it('should return 1 if EMPTY cell count 3 or 6', () => {
+      //
+    });
+  });
+});
