e2b9e4a388aaa4e8afc6b9ce9c9be3bdeb6219db
James Kim <yameskim@gmail.com>
2018-07-12 21:33:25 -0400

    Add testing for generateNum

--- test/test.js
+++ test/test.js
@@ -1,13 +1,15 @@
 // import React from 'react';
 import App from '../src/components/App';
-const assert = require('assert');
+import assert from 'assert';
+import { expect } from 'chai';
 
 describe('Function', () => {
+  const app = new App;
   describe('Brain', () => {
     const arr = [[1,1,1],
                  [null,1,1],
                  [null,null,null]];
-    const app = new App;
+
     it('should return null if POPULATED cell fewer than 2', () => {
       // expect(test.brain(0,0,arr)).toEqual(null);
       assert.equal(app.brain(2,0,arr), null);
@@ -22,9 +24,23 @@ describe('Function', () => {
       assert.equal(app.brain(1,0,arr), 1);
     });
   });
+  describe('GenerateNum', () => {
+    it('shoud return a number greater than 0', () => {
+      expect(app.generateNum()).to.be.above(0);
+    });
+    it('shoud return a number less than than 5', () => {
+      // assert.isAbove(app.generateNum(), 0);
+      expect(app.generateNum()).to.be.below(5);
+    });
+  });
+  // describe('', => {
+  //   it('', () => {
+  //
+  //   });
+  // });
 });
 
-// Had Issues with compiling CSS but added ignore-styles
+// Had issues with compiling CSS but added ignore-styles
 // function noop() {
 //   return null;
 // }
